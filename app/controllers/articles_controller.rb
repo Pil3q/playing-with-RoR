90093c3c7e427a43a5c8a0c0a0a7e37498b1a46d
Patryk Pilecki <ppilecki@icloud.com>
2018-05-10 10:29:59 +0100

    destroying comment, destroyed article destroys the comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "Patrick", password: "not_encrypted1",
+  except: [:index, :show]
+
   def index
     @articles = Article.all
   end
